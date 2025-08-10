8859aaf63fce3881dd5215fdca2b3e1117bb12f2
will <will.bradshaw50@gmail.com>
2025-08-10 14:44:21 +1000

    feat: solve equalinif

--- projects/equalinif/main.c
+++ projects/equalinif/main.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
   int class_size;
   printf("Enter class size: ");
   scanf("%d", &class_size);
-  if (class_size = MAX_CLASS_SIZE) {
+  if (class_size >= MAX_CLASS_SIZE) {
     printf("Class is now full\n");
   } else {
     printf("More students can be accepted\n");
